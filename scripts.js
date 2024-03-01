b103396b22d0b0c04994743bd65c402746db20bd
Jordan Williams <jordanw0412@gmail.com>
2024-03-01 03:35:56 -0500

    added DOM manipulations that updates country names from API

--- scripts.js
+++ scripts.js
@@ -1,11 +1,30 @@
 const startClick = document.getElementById("start-button");
 const game = document.getElementById("game-section")
 
-const toMainMenu = () =>{
+const toMainMenu = async() =>{
     game.replaceChildren()
     const selectCountryBox = document.createElement("div")
+    const countryListSelectionBox = document.createElement("ul")
+    
+
+    let response = await fetch("https://restcountries.com/v3.1/all")
+    let data = await response.json()
+
+    
+    console.log(data[0].name.official)
+    
+
+    data.map((country)=>{
+        const countryListItem = document.createElement("li")
+        countryListItem.innerText = country.name.official
+        countryListSelectionBox.appendChild(countryListItem)
+    })
+    
     selectCountryBox.innerText = "Select A country"
+    selectCountryBox.id = "select-country-box"
+
     game.appendChild(selectCountryBox)
+    game.appendChild(countryListSelectionBox)
 }
 
 startClick.addEventListener("click", () => {
