3905d102f747f10c7b67e53ae59073a9c9010fec
Jordan Williams <jordanw0412@gmail.com>
2024-03-01 14:05:32 -0500

    added sorted country name list from API with DOM manipulation

--- scripts.js
+++ scripts.js
@@ -5,22 +5,27 @@ const toMainMenu = async() =>{
     game.replaceChildren()
     const selectCountryBox = document.createElement("div")
     const countryListSelectionBox = document.createElement("ul")
+
+    countryListSelectionBox.id = "country-list-selection"
     
 
     let response = await fetch("https://restcountries.com/v3.1/all")
     let data = await response.json()
 
-    
-    console.log(data[0].name.official)
-    
-
-    data.map((country)=>{
+    let countryNames = []
+    for(i = 0; i < data.length; i++){
+        countryNames.push(data[i].name.official)
+        console.log(data[i].name.official)
+    }
+    countryNames.sort()
+    countryNames.map((country) => {
         const countryListItem = document.createElement("li")
-        countryListItem.innerText = country.name.official
+        countryListItem.innerText = country
         countryListSelectionBox.appendChild(countryListItem)
+        console.log(countryListItem)    
     })
     
-    selectCountryBox.innerText = "Select A country"
+    selectCountryBox.innerText = "Select A Country"
     selectCountryBox.id = "select-country-box"
 
     game.appendChild(selectCountryBox)
