4819f40532d3cf684d951f62f95bc9659c556269
Jordan Williams <jordanw0412@gmail.com>
2024-03-02 13:59:55 -0500

    added logic that saves user's selected country that then navigates to the main menu with the saved country

--- scripts.js
+++ scripts.js
@@ -1,22 +1,29 @@
 const startClick = document.getElementById("start-button");
 const game = document.getElementById("game-section")
-let currentCounty = ""
+let selectedCountry = ""
 
-const toMainMenu = async() =>{
+const toMainMenu = () =>{
+    game.replaceChildren()
+    const countryBox = document.createElement("div")
+    countryBox.innerHTML = selectedCountry
+    game.appendChild(countryBox)
+}
+
+// Funtion that navigates to country selection after clicking start
+const toCountrySelection = async() =>{
     game.replaceChildren()
     const selectCountryBox = document.createElement("div")
     const countryListSelectionBox = document.createElement("ul")
-
     countryListSelectionBox.id = "country-list-selection"
-    
 
     let response = await fetch("https://restcountries.com/v3.1/all")
     let data = await response.json()
 
+    // makes a new array with all country names to have access to the .sort method 
+    // that lists countries alphabetically
     let countryNames = []
     for(i = 0; i < data.length; i++){
         countryNames.push(data[i].name.official)
-        console.log(data[i].name.official)
     }
     countryNames.sort()
     countryNames.map((country) => {
@@ -24,7 +31,13 @@ const toMainMenu = async() =>{
         countryListItem.id = "country-list-item"
         countryListItem.innerText = country
         countryListSelectionBox.appendChild(countryListItem)
-        console.log(countryListItem)    
+        
+        //when user clicks on a country it saves it
+        // then navigates to main menu
+        countryListItem.addEventListener("click", ()=>{
+            selectedCountry = country
+            toMainMenu()
+        }) 
     })
     
     selectCountryBox.innerText = "Select A Country"
@@ -34,10 +47,6 @@ const toMainMenu = async() =>{
     game.appendChild(countryListSelectionBox)
 }
 
-countryListSelectionBox.addEventListener("click", (e)=>{
-
-})
-
 startClick.addEventListener("click", () => {
-    toMainMenu()
+    toCountrySelection()
 });
